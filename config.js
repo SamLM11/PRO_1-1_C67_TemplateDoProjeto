cd07a95ab86068901ea1747701986e3670a32c03
SamLM11 <samuellm2011@gmail.com>
2023-07-23 20:50:20 -0300

    alteração da interface do App

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// adicione SDK do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBPnYZ4RO4nR4SabGKHhBbcaKf8tnan73Y",
+    authDomain: "projeto67-5d502.firebaseapp.com",
+    projectId: "projeto67-5d502",
+    storageBucket: "projeto67-5d502.appspot.com",
+    messagingSenderId: "787538909919",
+    appId: "1:787538909919:web:005c5fe4c26ccab62fa76a"
+  };
+// Initialice o Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
